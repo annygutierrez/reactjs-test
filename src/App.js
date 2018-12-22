e5820123405a35dd47f761fabccb39b9700682a8
Anny Guttierrez <agutilop@everis.com>
2018-12-22 18:22:58 -0500

    Understanding & using state
    In this commit we started using state to show the names and ages
    dinamically.  Now we know that state is a special property that classes
    that are extended with component have, and when it changes anywhere, it
    leads to react to re-render the component.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Anny", age: 20 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, Im a React app</h1>
         <p>This is really working</p>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My hobbies: Racing</Person> 
-        <Person name="Anny" age="20"/>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person> 
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Im a React App!!!'));
