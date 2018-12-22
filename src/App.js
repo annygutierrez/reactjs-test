d495451592a5827f281212f29d6b18058aa12f4b
Anny Guttierrez <agutilop@everis.com>
2018-12-22 17:18:15 -0500

    Understanding JSX
    In this commit we are explaining the 'behind scenes of React components
    and syntactical sugar that we are using as HTML, how it really works,
    and why we use it insted of the React functions'

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
         <h1>Hi, Im a React app</h1>
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Im a React App!!!'));
   }
 }
 
