e97cc06948e7f7ccadfe805ec60ebb30f7c3cfde
Anny Guttierrez <agutilop@everis.com>
2018-12-22 19:21:18 -0500

    Passing method references between components
    We learn that we can pass methods directly inside of an arrow function
    to access to the this, or pass it like a props but, executing the bind
    function and passing the this like a prop, Max recomend to use the bind
    function, cause the other one not always work.

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked')
     // DON'T DO THIS this.state.persons[0].name = "Maximillian";
     this.setState({ 
       persons: [
-        { name: "Maximilian", age: 28 },
+        { name: newName, age: 28 },
         { name: "Manu", age: 29 },
         { name: "Anny", age: 21 }
       ] 
@@ -29,10 +29,20 @@ class App extends Component {
       <div className="App">
         <h1>Hi, Im a React app</h1>
         <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person> 
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+        />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+        >My hobbies: Racing</Person> 
+        <Person 
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Im a React App!!!'));
