f51d6f7b269f6ff3a2960bc91e2c1f37bcf003fe
Anny Guttierrez <agutilop@everis.com>
2018-12-31 18:19:32 -0500

    Add radium dependency
    Add radium to work with pseudo styles and media-queries.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -53,7 +54,11 @@ class App extends Component {
       color: 'white',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -75,6 +80,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
@@ -104,4 +113,5 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
+
