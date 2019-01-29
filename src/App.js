8290c9c41d357e4c3b168034d0c3b7190e600b82
Anny Guttierrez <agutilop@everis.com>
2019-01-28 23:00:17 -0500

    Get rid of radium
    - Delete radium form all the components and containers.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
@@ -54,11 +53,7 @@ class App extends Component {
       color: 'white',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
+      cursor: 'pointer'
     };
 
     let persons = null;
@@ -80,10 +75,6 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
     }
 
     const classes = [];
@@ -97,23 +88,21 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi, Im a React app</h1>
-          <p className={classes.join(' ')}>This is really working</p>
-          <button
-            style={style}
-            onClick={this.togglePersonsHandler}
-          >Toggle Persons</button>
-          {
-            persons
-          }
-        </div>
-      </StyleRoot>
+      <div className="App">
+        <h1>Hi, Im a React app</h1>
+        <p className={classes.join(' ')}>This is really working</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}
+        >Toggle Persons</button>
+        {
+          persons
+        }
+      </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Im a React App!!!'));
   }
 }
 
-export default Radium(App);
+export default App;
 
