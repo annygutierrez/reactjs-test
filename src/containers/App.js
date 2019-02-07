aa5b421fefd69c02897739da832c4a1805684389
Anny Guttierrez <agutilop@everis.com>
2019-02-06 21:40:47 -0500

    Improve the structure of the project
    - Add new stateless components and clean the code to be mantainable.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -48,46 +49,22 @@ class App extends Component {
 
   render() {
     let persons = null;
-    let btnClass = '';
 
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          {
-            this.state.persons.map((person, index) => {
-              return <Person
-                key={person.id}
-                click={() => { this.deletePersonHandler(index) }}
-                name={person.name}
-                age={person.age}
-                changed={(event) => { this.nameChangedHandler(event, person.id) }}
-              />
-            })
-          }
-        </div>
-      );
-
-      btnClass = classes.Red;
-    }
-
-    const assignClasses = [];
-
-    if (this.state.persons.length <= 2) {
-      assignClasses.push(classes.red); // classes = ['red'];
-    }
-
-    if (this.state.persons.length <= 1) {
-      assignClasses.push(classes.bold); // classes = ['red', 'bold'];
+      persons = <Persons
+        persons={this.state.persons}
+        clicked={this.deletePersonHandler}
+        changed={this.nameChangedHandler}
+      />;
     }
 
     return (
       <div className={classes.App}>
-        <h1>Hi, Im a React app</h1>
-        <p className={assignClasses.join(' ')}>This is really working</p>
-        <button
-          className={btnClass}
-          onClick={this.togglePersonsHandler}
-        >Toggle Persons</button>
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+        />
         {
           persons
         }
