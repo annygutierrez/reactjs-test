d29e447dbdeeb50b5ded6b7f6c9d94222b6c8aba
Anny Guttierrez <agutilop@everis.com>
2019-03-08 00:28:01 -0500

    Update react and react DOM version
    - Update react and reactDOM version.
    - Add reactHooks(useEffetc)
    
     Please enter the commit message for your changes. Lines starting

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+  
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    // Http request ...
+  });
+
   const assignClasses = [];
   let btnClass = '';
   if (props.showPersons) {
