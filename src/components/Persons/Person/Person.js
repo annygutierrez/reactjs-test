b0407f25f660aea54e801e522a6bd4444f00a91b
Anny Guttierrez <agutilop@everis.com>
2019-04-07 17:45:10 -0500

    Add Aux high order components
    - Aux HOC wraps all the content of the component and it allows us to
    return several elements at the same level, without wrapping all of them
    inside a div or something.
    . Note: If you are working with windows you shouldn't name the HOC
    components folder 'Aux' instead use Auxiliar.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux';
 import classes from './Person.css';
 
 class Person extends Component {
   render() {
     return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-        <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} defaultValue={this.props.name} />
-      </div>
+      <Aux>
+        <div className={classes.Person}>
+          <p onClick={this.props.click}>
+            I'm {this.props.name} and I am {this.props.age} years old!
+        </p>
+          <p>{this.props.children}</p>
+          <input
+            type="text"
+            onChange={this.props.changed}
+            defaultValue={this.props.name}
+          />
+        </div>
+      </Aux>
     );
   }
 };
