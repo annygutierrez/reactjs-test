83b76a6ba73c7a3b34d1a401a17f78b9453c43bb
Anny Guttierrez <agutilop@everis.com>
2019-04-07 17:50:59 -0500

    Using React.Fragment component
    - Wrap elements inside the Fragment component from React.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,7 +5,7 @@ import classes from './Person.css';
 class Person extends Component {
   render() {
     return (
-      <Aux>
+      <React.Fragment>
         <div className={classes.Person}>
           <p onClick={this.props.click}>
             I'm {this.props.name} and I am {this.props.age} years old!
@@ -17,7 +17,7 @@ class Person extends Component {
             defaultValue={this.props.name}
           />
         </div>
-      </Aux>
+      </React.Fragment>
     );
   }
 };
