1c4622e8787a9fce92cc6ea26c766603b70f701b
Anny Guttierrez <agutilop@everis.com>
2019-04-07 17:58:33 -0500

    Add withClass component
    - Add withClass component to wrap another component (Second example of
    high order components use)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,7 +5,7 @@ import classes from './Person.css';
 class Person extends Component {
   render() {
     return (
-      <React.Fragment>
+      <Aux>
         <div className={classes.Person}>
           <p onClick={this.props.click}>
             I'm {this.props.name} and I am {this.props.age} years old!
@@ -17,7 +17,7 @@ class Person extends Component {
             defaultValue={this.props.name}
           />
         </div>
-      </React.Fragment>
+      </Aux>
     );
   }
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
   state = {
@@ -74,7 +75,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => { this.setState({ showCockpit: false }) }}>Remove Cockpit</button>
         {
           this.state.showCockpit ? <Cockpit
@@ -87,7 +88,7 @@ class App extends Component {
         {
           persons
         }
-      </div>
+      </WithClass>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Im a React App!!!'));
   }
