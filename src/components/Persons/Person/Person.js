57a470e5c8ee93991ede18217e7be8ffc046ebb8
Anny Guttierrez <agutilop@everis.com>
2019-04-09 00:07:06 -0500

    Using static context and useContext
    - Using context in class based components/ static contextType
    - Using context in functional component / useContext from React Hooks

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -12,20 +12,19 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     // This kind of ref only works in class based components, not in functional ones.
     // this.inputElement.focus();
-    this.inputElementRef.current.focus()
+    this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
   }
 
   render() {
     return (
       <Aux>
-        <AuthContext.Consumer>
-         {
-           (context) => context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>
-         }
-        </AuthContext.Consumer>
+        {this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
